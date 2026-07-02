ca079dff04199dfff4eef325341317840a0cd245
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-07-02 15:11:28 +1200

    rename 'legal info' to 'important info'

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer>
-    <p><a href="/legal">Legal info</a>. <a href="/about">About this app</a>.</p>
+    <p><a href="/important-info">Important info</a>. <a href="/about">About this app</a>.</p>
     <p>Built by <a href="https://oweltonrosie.com" target="_blank" rel="noopener noreferrer">Oscar Welton-Rosie</a>. Source code on <a href="https://github.com/OWelton-Rosie/regs-helper" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
     <p><a href="admin">Admin login</a></p>
 </footer>
